f5e60625360bcfd1547932d0c066c012c3784721
Hewitt <wangh3@bxscience.edu>
2025-03-25 23:52:11 -0400

    first commit

---
+++ src/chatterbot_test.py
@@ -0,0 +1,63 @@
+import os
+import discord
+import time
+import asyncio
+import random
+from dotenv import load_dotenv
+from discord.channel import DMChannel
+import ssl 
+
+from chatterbot import ChatBot
+
+from chatterbot.trainers import ListTrainer
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+DATA_CHANNEL = int(os.getenv("DATA_CHANNEL"))
+DM_CHANNEL = int(os.getenv("DM_CHANNEL"))
+
+chatbot = ChatBot("danielpark")
+
+class MyClient(discord.Client):
+    async def on_message(self, message: discord.Message):
+        if message.author.id == self.user.id:
+            return
+
+        if message.channel.id == DM_CHANNEL:
+            response = chatbot.get_response(message.content)
+            print(f"replying {response}")
+            message = await message.reply(content=response)
+            print(f"sent {message}");
+
+async def main():
+    client = MyClient()
+
+    await client.login(TOKEN)
+
+    channel: DMChannel = await client.fetch_channel(DATA_CHANNEL)
+
+    trainer = ListTrainer(chatbot)
+
+    messages: list[str] = []
+
+    messages_file = f"data/{DATA_CHANNEL}.txt"
+
+    if not os.path.isfile(messages_file):
+        print("scraping channel")
+        async for message in channel.history(limit=4000):
+            messages.append(message.content)
+
+        with open(messages_file, "a", encoding="utf-8") as file:
+            for message in messages:
+                file.write(message + "\n")
+                
+    else:
+        with open(messages_file, "r", encoding="utf-8") as file:
+            messages = file.readlines()
+        
+
+    trainer.train(messages)
+
+    await client.connect()
+
+asyncio.run(main())
